747789274c13836333270e55818eb310a3da3e33
Oliver Fleckenstein <oliverfl@live.dk>
2023-11-30 10:31:06 +0000

    refactor: Idomitic go

--- client/client.go
+++ client/client.go
@@ -23,19 +23,19 @@ const AOC_TOKEN_NAME = "AOC_TOKEN"
 
 // Get the input for a given year and day.
 func GetInput(year Year, day Day) string {
-	var directory = cacheLocation(year)
-	var filename = filepath.Join(directory, fmt.Sprintf("%d.txt", day))
+	directory := cacheLocation(year)
+	filename := filepath.Join(directory, fmt.Sprintf("%d.txt", day))
 
 	if _, err := os.Stat(filename); err == nil {
 		// log.Println("Reading file")
-		var input, err = os.ReadFile(filename)
+		input, err := os.ReadFile(filename)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		return string(input)
 	} else if errors.Is(err, os.ErrNotExist) {
-		var input = fetchInput(year, day)
+		input := fetchInput(year, day)
 		os.MkdirAll(directory, os.ModePerm)
 		if err := os.WriteFile(filename, []byte(input), 0666); err != nil {
 			log.Fatalf("Failed to write file: %s", err)
@@ -100,23 +100,27 @@ func cacheLocation(year Year) string {
 
 // Fetch the input for a problem, given a year and day.
 func fetchInput(year Year, day Day) string {
-	var url = fmt.Sprintf("%s/input", baseUrl(year, day))
-	var request, err = http.NewRequest(http.MethodGet, url, nil)
+	url := fmt.Sprintf("%s/input", baseUrl(year, day))
+	request, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		log.Fatalf("Failed to create request %s", err)
 	}
 	authenticate(request)
 
-	var response, _ = client.Do(request)
+	response, err := client.Do(request)
+	if err != nil {
+		panic(err)
+	}
+
 	defer response.Body.Close()
 
-	var body, _ = io.ReadAll(response.Body)
+	body, _ := io.ReadAll(response.Body)
 
 	return string(body)
 }
 
 func authenticate(request *http.Request) {
-	var token, has_token = os.LookupEnv(AOC_TOKEN_NAME)
+	token, has_token := os.LookupEnv(AOC_TOKEN_NAME)
 	if !has_token {
 		log.Panicf("Missing `%s` in environment", AOC_TOKEN_NAME)
 	}
